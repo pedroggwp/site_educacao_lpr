bebcf3aad6582d3ed7eeffd282a7fa067e820712
pedroggwp <phsnnascimento@outlook.com>
2024-05-14 11:23:10 -0300

    Subindo home completo

---
+++ home/home.js
@@ -0,0 +1,13 @@
+var url = window.location.pathname;
+var filename = url.substring(url.lastIndexOf('/')+1);
+
+filename = filename.replace(/\.[^/.]+$/, "");
+
+confirm
+
+document.querySelectorAll('.menu a').forEach(function(element) {
+    console.log(element)
+    if (element.getAttribute('href') === filename + '.html') {
+      element.parentNode.classList.add('active');
+    }
+  });
