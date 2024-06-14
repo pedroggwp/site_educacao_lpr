b14a59a756d2602157a0b5d57f2e1d89ff2d5bc3
Biancavitoriav <124395956+Biancavitoriav@users.noreply.github.com>
2024-06-14 09:16:10 -0300

    subindo links

--- home/home.js
+++ home/home.js
@@ -6,7 +6,6 @@ filename = filename.replace(/\.[^/.]+$/, "");
 console.log(filename)
 
 document.querySelectorAll('.menu a').forEach(function(element) {
-    // console.log(element)
 
     console.log(element.getAttribute('href'))
     console.log(filename)
