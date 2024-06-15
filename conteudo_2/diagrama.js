888b34fc55f9702e57ba6862ef046de1e930e166
TabuchiEnzo <enzo.tabuchi@germinare.org.br>
2024-06-14 21:03:20 -0300

    diagrama melhorias

--- conteudo_2/diagrama.js
+++ conteudo_2/diagrama.js
@@ -3,13 +3,13 @@ const posicoes = document.getElementsByClassName("posicaoBloco");
 
 let blocoAtual = null;
 
-// Função para restaurar o bloco para a lista original
+// função para resetar o bloco
 function resetarBloco(bloco) {
     lugar = document.getElementById("blocosDisponiveis")
     lugar.appendChild(bloco)
 }
 
-// Função para lidar com o início do arrasto
+// função para deixar o bloco ser arrastado
 function handleDragStart(e) {
     blocoAtual = e.target;
     e.dataTransfer.setData("text/plain", e.target.id);
@@ -18,19 +18,19 @@ function handleDragStart(e) {
     }, 0);
 }
 
-// Função para lidar com o fim do arrasto
+// função que se ele errar o bloco de lugar ele volta para o normal 
 function handleDragEnd(e) {
     setTimeout(() => {
-        blocoAtual = null;
+        blocoAtual.style.display = "flex";
     }, 0);
 }
 
-// Função para permitir soltar na posição
+// função que deixar soltar o bloco em uma das opções
 function handleDragOver(e) {
     e.preventDefault();
 }
 
-// Função para lidar com a ação de soltar
+// função quando soltar o bloco
 function handleDrop(e) {
     e.preventDefault();
     const blocoId = e.dataTransfer.getData("text");
@@ -42,7 +42,6 @@ function handleDrop(e) {
         e.target.appendChild(bloco);
         bloco.style.margin = "auto"; 
         bloco.style.padding = "0"; 
-        bloco.style.fontSize = "12px";
         bloco.style.display = "flex";
         bloco.style.justifyContent = "center";
         bloco.style.alignItems = "center";
@@ -52,14 +51,14 @@ function handleDrop(e) {
     }
 }
 
-// Adiciona eventos de arrastar e soltar para cada bloco
+// eventos de arrastar e soltar para cada bloco
 for (let i = 0; i < blocos.length; i++) {
     blocos[i].draggable = true;
     blocos[i].ondragstart = handleDragStart;
     blocos[i].ondragend = handleDragEnd;
 }
 
-// Adiciona eventos de arrastar e soltar para cada posição
+// eventos de arrastar e soltar para cada poisção
 for (let i = 0; i < posicoes.length; i++) {
     posicoes[i].ondragover = handleDragOver;
     posicoes[i].ondrop = handleDrop;
