49be1821d6b8b63e466503b3813f0373102b748f
Biancavitoriav <124395956+Biancavitoriav@users.noreply.github.com>
2024-06-17 07:54:29 -0300

    Arrumando a verificação do input

--- conteudo_3/index.js
+++ conteudo_3/index.js
@@ -9,9 +9,16 @@ const input = document.getElementById("input")
 bt_submit.addEventListener("click", async function (event) {
     event.preventDefault(); //Previnindo que a pagina não recarregue ao enviar formulario
     const codigo = document.getElementById("input").value;
-    if (document.getElementById("input").textContent.includes("input")){
-      
+    let validador = codigo.split("(")[1].split(")")[0]
+    if(validador.startsWith('"')){
+      validador = validador.replace('"',"")
+      validador = validador.replace('"',"")
     }
+    else{
+      validador = validador.replace("'","");
+      validador = validador.replace("'","");
+    }
+    console.log(validador)
     try {
       //Indicando o caminho até o script python, /execute é o caminho q passei no script
       const requisicao = await fetch("https://flask-api-dad.onrender.com/execute", {
@@ -26,9 +33,10 @@ bt_submit.addEventListener("click", async function (event) {
       });
       //Pegando o resultado da requisição
       const resultado = await requisicao.json();
+      let resultadoValida = resultado.output.replace(validador,"")
       console.log("resultado.output"+resultado.output)
       //Coloquei o resultado previsto como "hello world" de exemplo
-      if(resultado.output.trim().toLowerCase() === "analista de dados" && input.textContent().toLowerCase().contains("input")){
+      if(resultadoValida.trim().toLowerCase() === "analista de dados" && input.value.toLowerCase().includes("input")){
         modal_certo.showModal();
         console.log("caiu no modal certo");
       }else{
